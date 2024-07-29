cc67fcbbf5be1a0a39407813a963125797bd8473
matheusfabiao <matheusfabiaopereira@gmail.com>
2024-07-29 13:30:38 -0300

    feat: added footer & carousel

--- js/main.js
+++ js/main.js
@@ -76,6 +76,34 @@ window.addEventListener('load', ()=>{
             setTimeout(()=>{
                 loaderContainer.style.display = 'none'
             }, 500)
-        }, 1000)
+        }, 0) //mudar para 1000
     }
 })
+
+// CAROUSEL
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 2
+        },
+        1000: {
+          items: 3
+        },
+        1200: {
+          items: 4
+        },
+        1400: {
+          items: 5
+        }
+      }
+    })
+  })
